02e628d6204f53685d68945451f80f239094b404
neringa <neringa.bickmore@gmail.com>
2020-10-04 20:59:19 +0100

    Add virtual environment

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "0d/imBZL'0TFjDFV4[_1xLdFe6b")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "scribbles")
