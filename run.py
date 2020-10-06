89fe7d0962c2075ad19f05d28b398dbca9f902f2
neringa <neringa.bickmore@gmail.com>
2020-10-06 22:43:42 +0100

    Add register functionality to the app

--- run.py
+++ run.py
@@ -25,6 +25,30 @@ def about():
     return render_template("pages/about.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            { "username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        return redirect(url_for("discover", username=session["user"]))
+
+    return render_template("components/forms/register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")), 
